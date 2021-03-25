d001ef13748403854359e4f78f0231d10bab2583
Domagoj Zganec <domagoj.zganec1@gmail.com>
2021-03-25 14:39:12 +0100

    fix: showing adblock notice properly

--- src/scenes/NewTab/components/Ads/index.tsx
+++ src/scenes/NewTab/components/Ads/index.tsx
@@ -9,6 +9,7 @@ import { DonationsInfoContext } from "services/DonationsInfo";
 import { keys } from "consts/localStorage";
 
 import { AdsWrapper, AdblockNotice } from "./index.styled";
+import { useToggle } from "utils/useToggle";
 
 const Iframe = ({ src }: any) => (
   <iframe
@@ -21,6 +22,7 @@ const Iframe = ({ src }: any) => (
 
 const Ads = () => {
   const [shownAds, setShownAds] = useState<Array<any> | null>(null);
+  const adBlock = useToggle();
   const {
     state: { donationsInfoData },
     loadDonationsInfo,
@@ -36,6 +38,13 @@ const Ads = () => {
       } else {
         loadDonationsInfo();
       }
+
+      // hacky way to check if adblock is turned on
+      setTimeout(() => {
+        if (adblockDetector.current?.offsetHeight === 0) {
+          adBlock.setOn();
+        }
+      }, 500);
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     []
@@ -58,8 +67,6 @@ const Ads = () => {
   const ad1Src = `//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=DE&source=ac&ref=qf_br_asin_til&ad_type=product_link&tracking_id=zanas-21&marketplace=amazon&region=DE&placement=${shownAds[0].placement}&asins=${shownAds[0].placement}&linkId=${shownAds[0].linkId}&show_border=false&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff`;
   const ad2Src = `//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=DE&source=ac&ref=qf_br_asin_til&ad_type=product_link&tracking_id=zanas-21&marketplace=amazon&region=DE&placement=${shownAds[1].placement}&asins=${shownAds[1].placement}&linkId=${shownAds[1].linkId}&show_border=false&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff`;
 
-  const isUsingAdBlock = adblockDetector.current?.offsetHeight === 0;
-
   return (
     <AdsWrapper>
       <Stack direction="column" spacing="small">
@@ -76,7 +83,7 @@ const Ads = () => {
           style={{ border: "none" }}
           ref={adblockDetector}
         ></iframe>
-        {isUsingAdBlock && (
+        {adBlock.isOn && (
           <AdblockNotice>
             <Alert type="warning">
               {translate("problem_showing_ads")}
