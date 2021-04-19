a2cbab6b91d8e9edc176c023f7339f08027a1094
Domagoj Zganec <domagoj.zganec1@gmail.com>
2021-04-19 13:12:45 +0200

    feat: bookmark href parent

--- src/scenes/NewTab/components/Bookmarks/Bookmark.tsx
+++ src/scenes/NewTab/components/Bookmarks/Bookmark.tsx
@@ -12,8 +12,6 @@ import {
 } from "./index.styled";
 import { useToggle } from "utils/useToggle";
 
-const mouseClicks = ["_parent", "_blank"];
-
 const Bookmark = ({
   bookmark,
   deleteBookmark,
@@ -31,14 +29,6 @@ const Bookmark = ({
   const handleDeleteClick = () => {
     deleteBookmark(bookmark.id);
     isPopoverOpen.setOff();
-  };
-
-  const handleBookmarkClick = (url: string) => (event: any) => {
-    const parsedURL = url.startsWith("http") ? url : `http://${url}`;
-
-    // left click = same tab, middle click = new tab
-    const windowOpenType = mouseClicks[event.button];
-    windowOpenType && window.open(parsedURL, windowOpenType);
   };
 
   return (
@@ -59,7 +49,14 @@ const Bookmark = ({
           <MenuMeatballsIcon customColor="#1f7bb6" size="small" />
         </Popover>
       </OptionsWrapper>
-      <ExternalLink onMouseDown={handleBookmarkClick(bookmark.url)}>
+      <ExternalLink
+        target="_parent"
+        href={
+          bookmark.url.startsWith("http")
+            ? bookmark.url
+            : `http://${bookmark.url}`
+        }
+      >
         <Button>{bookmark.name}</Button>
       </ExternalLink>
     </BookmarkWrapper>
